a3516f53e56c18f046321450be007599d56eb847
MurariKumarThakur <murarirai.one@gmail.com>
2018-03-27 09:21:48 +0530

    added testcase

--- src/main/java/com/action/engine/ActionEngine.java
+++ src/main/java/com/action/engine/ActionEngine.java
@@ -1,7 +1,12 @@
 package com.action.engine;
 
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.base.engine.DriverEngine;
 
@@ -9,40 +14,65 @@ import com.helper.utility.GetExtentReport;
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.LogStatus;
 
-public class ActionEngine extends DriverEngine{
-	
-	
-	public static void navigateToUrl(String enterUrl){
-		
+public class ActionEngine extends DriverEngine {
+ public  static 	WebDriverWait wait;
+
+	public static void navigateToUrl(String enterUrl) {
+
 		driver.get(enterUrl);
-		
-		
-	}
-	
-	public static String getCurrentUrl(){
-		
-		
-	  return	driver.getCurrentUrl();
-	}
-	
-	
-	public static void click(WebElement name ){
-		
-	         name.click();
-	   
-	GetExtentReport.logger.log(LogStatus.INFO, "Clicked on Element");       
-		
+
 	}
 
-   public static String   getTitle(){
-	   
-	   return driver.getTitle(); 
-	   
+	public static String getCurrentUrl() {
+
+		return driver.getCurrentUrl();
+	}
+
+	public static void click(WebElement name) {
+
+		name.click();
+
+		GetExtentReport.logger.log(LogStatus.INFO, "Clicked on Element");
+
+	}
+
+	public static String getTitle() {
+
+		return driver.getTitle();
+
+	}
 	
-	   
-	  
-   }
+	public static void clear(WebElement name )
+	{
+		
+       name.clear();
+	}
+	
+	
+	
+	public static void sendKeys(WebElement name , String data ){
+		
+		
+		name.sendKeys(data);
+	}
+	
+	
+	public static  void waitForElementVisibility(WebElement locator) {
 
+		wait = new WebDriverWait(driver, 35);
+		wait.pollingEvery(5, TimeUnit.SECONDS);
+		wait.ignoring(NoSuchElementException.class);
+		wait.until(ExpectedConditions.visibilityOf(locator));
 
+	}
+
+	public static  void waitForElementClickable(WebElement locator) {
+
+		wait = new WebDriverWait(driver, 35);
+		wait.pollingEvery(5, TimeUnit.SECONDS);
+		wait.ignoring(NoSuchElementException.class);
+		wait.until(ExpectedConditions.elementToBeClickable(locator));
+
+	}
 
 }
--- src/test/java/com/tailorManStore/pages/LoginPage.java
+++ src/test/java/com/tailorManStore/pages/LoginPage.java
@@ -3,14 +3,51 @@
  */
 package com.tailorManStore.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+import com.action.engine.ActionEngine;
+
 /**
  * @author Murari
  *
  */
-public class LoginPage{
+public class LoginPage extends ActionEngine {
 	
 	
+	@FindBy(how =How.LINK_TEXT , using="login") 
+	   WebElement loginButton ;
 	
+	  
+	@FindBy(how=How.XPATH,using ="//h1[text()='Sign in']")
+        WebElement signInText ;
+	
+	@FindBy(how = How.ID,using="identifierId")
+	    WebElement userName ;
+	
+	@FindBy(how = How.XPATH,using="//span[text()='Next']")
+	WebElement nextButton;
+	
+	@FindBy(how = How.NAME,using ="password")
+	WebElement password;
+	
+	@FindBy (how =How.XPATH,using="//h1[text()='Welcome']")
+	 WebElement welcomeText;
 	 
-
+	 
+	
+	public void loginTailorManStore(String userNameValue , String passwordValue ){
+		
+		ActionEngine.click(loginButton);
+		ActionEngine.waitForElementVisibility(signInText);
+		ActionEngine.sendKeys(userName,userNameValue);
+		ActionEngine.click(nextButton);
+		ActionEngine.waitForElementVisibility(welcomeText);
+		ActionEngine.sendKeys(password, passwordValue);
+		ActionEngine.click(nextButton);
+		
+		
+	}
+	
 }
